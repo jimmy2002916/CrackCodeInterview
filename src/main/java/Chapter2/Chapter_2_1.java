7c760cfd075118339e8786e3bbd60b7b892ff8cf
Jimmy Huang <jimmy2002916@hotmail.com>
2020-01-02 21:52:21 +0800

    ch 2.1 not finished yet.

---
+++ src/main/java/Chapter2/Chapter_2_1.java
@@ -0,0 +1,31 @@
+package Chapter2;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+//https://www.geeksforgeeks.org/remove-duplicates-from-a-sorted-linked-list/
+
+public class Chapter_2_1 {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> object = new LinkedList<String>();
+        object.add("A");
+        object.add("B");
+        object.add("B");
+        object.add("C");
+        System.out.println("Linked list : " + object);
+
+    }
+
+    static void deleteDups(LinkedList n){
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        while( n != null){
+
+        }
+
+
+    }
+
+}
